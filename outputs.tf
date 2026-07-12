402ed7b3804ff4f5eb587439a5b226544a0813e3
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:02 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,33 @@
-output "role_management_policies" {
-  description = "All role_management_policy resources"
-  value       = azurerm_role_management_policy.role_management_policies
-}
 output "role_management_policies_activation_rules" {
-  description = "List of activation_rules values across all role_management_policies"
-  value       = [for k, v in azurerm_role_management_policy.role_management_policies : v.activation_rules]
+  description = "Map of activation_rules values across all role_management_policies, keyed the same as var.role_management_policies"
+  value       = { for k, v in azurerm_role_management_policy.role_management_policies : k => v.activation_rules }
 }
 output "role_management_policies_active_assignment_rules" {
-  description = "List of active_assignment_rules values across all role_management_policies"
-  value       = [for k, v in azurerm_role_management_policy.role_management_policies : v.active_assignment_rules]
+  description = "Map of active_assignment_rules values across all role_management_policies, keyed the same as var.role_management_policies"
+  value       = { for k, v in azurerm_role_management_policy.role_management_policies : k => v.active_assignment_rules }
 }
 output "role_management_policies_description" {
-  description = "List of description values across all role_management_policies"
-  value       = [for k, v in azurerm_role_management_policy.role_management_policies : v.description]
+  description = "Map of description values across all role_management_policies, keyed the same as var.role_management_policies"
+  value       = { for k, v in azurerm_role_management_policy.role_management_policies : k => v.description }
 }
 output "role_management_policies_eligible_assignment_rules" {
-  description = "List of eligible_assignment_rules values across all role_management_policies"
-  value       = [for k, v in azurerm_role_management_policy.role_management_policies : v.eligible_assignment_rules]
+  description = "Map of eligible_assignment_rules values across all role_management_policies, keyed the same as var.role_management_policies"
+  value       = { for k, v in azurerm_role_management_policy.role_management_policies : k => v.eligible_assignment_rules }
 }
 output "role_management_policies_name" {
-  description = "List of name values across all role_management_policies"
-  value       = [for k, v in azurerm_role_management_policy.role_management_policies : v.name]
+  description = "Map of name values across all role_management_policies, keyed the same as var.role_management_policies"
+  value       = { for k, v in azurerm_role_management_policy.role_management_policies : k => v.name }
 }
 output "role_management_policies_notification_rules" {
-  description = "List of notification_rules values across all role_management_policies"
-  value       = [for k, v in azurerm_role_management_policy.role_management_policies : v.notification_rules]
+  description = "Map of notification_rules values across all role_management_policies, keyed the same as var.role_management_policies"
+  value       = { for k, v in azurerm_role_management_policy.role_management_policies : k => v.notification_rules }
 }
 output "role_management_policies_role_definition_id" {
-  description = "List of role_definition_id values across all role_management_policies"
-  value       = [for k, v in azurerm_role_management_policy.role_management_policies : v.role_definition_id]
+  description = "Map of role_definition_id values across all role_management_policies, keyed the same as var.role_management_policies"
+  value       = { for k, v in azurerm_role_management_policy.role_management_policies : k => v.role_definition_id }
 }
 output "role_management_policies_scope" {
-  description = "List of scope values across all role_management_policies"
-  value       = [for k, v in azurerm_role_management_policy.role_management_policies : v.scope]
+  description = "Map of scope values across all role_management_policies, keyed the same as var.role_management_policies"
+  value       = { for k, v in azurerm_role_management_policy.role_management_policies : k => v.scope }
 }
 
